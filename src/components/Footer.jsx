6afdeb53f621d4c788a70f6313cb5f10a2c43ebe
Vidur Mehta <vidurmehta7@gmail.com>
2023-02-08 20:00:36 +0530

    updates2.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Logo from "..//img/BlogItLOGO.png";
 
 const Footer = () => {
-    return(
-        <div>Footer</div>
-    )
-}
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
