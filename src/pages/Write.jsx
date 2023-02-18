7afdc4369c3ed992dbf0d4e92beda330ac2ee338
Vidur Mehta <vidurmehta7@gmail.com>
2023-02-18 12:02:12 +0530

    frontend final

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -23,26 +23,32 @@ const Write = () => {
                         <b>Visibility: </b> Public
                     </span>
                     <input style={{display:"none"}} type="file" name="" id="file" />
-                    <label htmlFor="item">Upload Image</label>
+                    <label className="file" htmlFor="file">Upload Image</label>
                     <div className="buttons">
                         <button>Save as a Draft</button>
-                        <button>Update  </button>
+                        <button>Update</button>
                     </div>
                 </div>
                 <div className="item">
                     <h1>Category</h1>
+                    <div className="cat">
                     <input type="radio" name="cat" value="art" id="art" />
-                    <label htmlFor="art">ART</label>
+                    <label htmlFor="art">ART</label></div>
+                    <div className="cat">
                     <input type="radio" name="cat" value="science" id="science" />
-                    <label htmlFor="science">SCIENCE</label>
+                    <label htmlFor="science">SCIENCE</label></div>
+                    <div className="cat">
                     <input type="radio" name="cat" value="technology" id="technology" />
-                    <label htmlFor="technology">TECHNOLOGY</label>
+                    <label htmlFor="technology">TECHNOLOGY</label></div>
+                    <div className="cat">
                     <input type="radio" name="cat" value="cinema" id="cinema" />
-                    <label htmlFor="cinema">CINEMA</label>
+                    <label htmlFor="cinema">CINEMA</label></div>
+                    <div className="cat">
                     <input type="radio" name="cat" value="design" id="design" />
-                    <label htmlFor="design">DESIGN</label>
+                    <label htmlFor="design">DESIGN</label></div>
+                    <div className="cat">
                     <input type="radio" name="cat" value="food" id="food" />
-                    <label htmlFor="food">FOOD</label>
+                    <label htmlFor="food">FOOD</label></div>
                 </div>
             </div>
         </div>
